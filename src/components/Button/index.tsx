e6ffb517851927c225922a4dba4bd2deeeeb112a
gsantini-ube <gsantini@ubeswap.org>
2022-12-12 14:50:02 -0800

    v1.3.2

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -66,26 +66,26 @@ export const ButtonPrimary = styled(Base)`
 `
 
 export const ButtonLight = styled(Base)`
-  background-color: ${({ theme }) => theme.primary5};
-  color: ${({ theme }) => theme.primaryText1};
+  background-color: ${({ theme }) => lighten(0.28, theme.primary1)};
+  color: ${({ theme }) => lighten(0.01, theme.primary1)};
   font-size: 16px;
   font-weight: 500;
   &:focus {
-    box-shadow: 0 0 0 1pt ${({ theme, disabled }) => !disabled && darken(0.03, theme.primary5)};
-    background-color: ${({ theme, disabled }) => !disabled && darken(0.03, theme.primary5)};
+    box-shadow: 0 0 0 1pt ${({ theme, disabled }) => !disabled && lighten(0.24, theme.primary1)};
+    background-color: ${({ theme, disabled }) => !disabled && lighten(0.24, theme.primary1)};
   }
   &:hover {
-    background-color: ${({ theme, disabled }) => !disabled && darken(0.03, theme.primary5)};
+    background-color: ${({ theme, disabled }) => !disabled && lighten(0.24, theme.primary1)};
   }
   &:active {
-    box-shadow: 0 0 0 1pt ${({ theme, disabled }) => !disabled && darken(0.05, theme.primary5)};
-    background-color: ${({ theme, disabled }) => !disabled && darken(0.05, theme.primary5)};
+    box-shadow: 0 0 0 1pt ${({ theme, disabled }) => !disabled && lighten(0.2, theme.primary1)};
+    background-color: ${({ theme, disabled }) => !disabled && lighten(0.2, theme.primary1)};
   }
   :disabled {
     opacity: 0.4;
     :hover {
       cursor: auto;
-      background-color: ${({ theme }) => theme.primary5};
+      background-color: ${({ theme }) => lighten(0.28, theme.primary1)};
       box-shadow: none;
       border: 1px solid transparent;
       outline: none;
